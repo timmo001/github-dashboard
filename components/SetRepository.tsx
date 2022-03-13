5b3b1eaa05264e3fca5e7c1b8a5d344ca1529d88
timmo001 <timmo001@users.noreply.github.com>
2022-03-13 19:31:25 +0000

    Prettified Code!

--- components/SetRepository.tsx
+++ components/SetRepository.tsx
@@ -163,7 +163,8 @@ function SetRepository(): ReactElement {
       maxWidth="md"
       open
       scroll="body"
-      onClose={handleCloseSetRepository}>
+      onClose={handleCloseSetRepository}
+    >
       <DialogTitle>Change Repository</DialogTitle>
       {newRepository ? (
         <>
@@ -174,7 +175,8 @@ function SetRepository(): ReactElement {
                 xs={5}
                 container
                 alignContent="center"
-                justifyContent="flex-end">
+                justifyContent="flex-end"
+              >
                 <Timeline position="alternate">
                   {steps.map((step: Step, key: number) => (
                     <TimelineItem key={key}>
@@ -198,7 +200,8 @@ function SetRepository(): ReactElement {
                 xs
                 container
                 alignContent="center"
-                justifyContent="center">
+                justifyContent="center"
+              >
                 {currentStep.label === "Type" ? (
                   <Autocomplete
                     disableClearable
@@ -271,7 +274,8 @@ function SetRepository(): ReactElement {
         <Grid
           container
           alignContent="space-around"
-          justifyContent="space-around">
+          justifyContent="space-around"
+        >
           <CircularProgress color="primary" />
         </Grid>
       )}
