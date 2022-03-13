51642ac46e57aded4a59ed02331044c498fbcf73
timmo001 <timmo001@users.noreply.github.com>
2022-03-13 19:55:57 +0000

    Prettified Code!

--- pages/index.tsx
+++ pages/index.tsx
@@ -260,7 +260,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
         container
         direction="row"
         alignContent="space-around"
-        justifyContent="center">
+        justifyContent="center"
+      >
         {alert ? (
           <Grid item xs={11}>
             <Alert severity="error">{alert}</Alert>
@@ -277,7 +278,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
               sx={{ padding: theme.spacing(1), marginBottom: theme.spacing(2) }}
               onClick={() => {
                 router.push(authorizeUrl);
-              }}>
+              }}
+            >
               Authenticate with GitHub
             </Button>
           </Grid>
@@ -289,14 +291,16 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
           xs={10}
           sx={{
             padding: theme.spacing(0),
-          }}>
+          }}
+        >
           {repositoryData ? (
             <>
               <Grid
                 container
                 direction="row"
                 alignContent="space-around"
-                justifyContent="space-around">
+                justifyContent="space-around"
+              >
                 <Grid item sx={{ padding: theme.spacing(1, 2) }}>
                   <Typography variant="h4" noWrap>
                     Discussions
@@ -353,7 +357,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
                 direction="row"
                 alignContent="space-around"
                 justifyContent="space-around"
-                sx={{ margin: theme.spacing(2, 0) }}>
+                sx={{ margin: theme.spacing(2, 0) }}
+              >
                 {repositoryData.release?.name ||
                 repositoryData.refs?.tags[0]?.name ? (
                   <Grid item sx={{ padding: theme.spacing(1, 2) }}>
@@ -376,7 +381,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
                     <Typography
                       variant="h5"
                       noWrap
-                      color={repositoryData.primaryLanguage.color}>
+                      color={repositoryData.primaryLanguage.color}
+                    >
                       {repositoryData.primaryLanguage?.name}
                     </Typography>
                   </Grid>
@@ -390,7 +396,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
                 direction="row"
                 alignContent="space-around"
                 justifyContent="space-around"
-                sx={{ margin: theme.spacing(2, 0) }}>
+                sx={{ margin: theme.spacing(2, 0) }}
+              >
                 <Grid item sm={12} lg={6} sx={{ padding: theme.spacing(1, 2) }}>
                   <Typography variant="h4" noWrap gutterBottom>
                     Open Issues by Day
@@ -399,7 +406,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
                     style={{
                       width: "100%",
                       height: 520,
-                    }}>
+                    }}
+                  >
                     <ResponsiveContainer width="100%" height="100%">
                       <LineChart data={issuesByDay}>
                         <XAxis dataKey="date" />
@@ -427,7 +435,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
                     style={{
                       width: "100%",
                       height: 520,
-                    }}>
+                    }}
+                  >
                     <ResponsiveContainer width="100%" height="100%">
                       <LineChart data={pullRequestsByDay}>
                         <XAxis dataKey="date" />
@@ -453,7 +462,8 @@ function Dashboard({ clientId }: DashboardProps): ReactElement {
             <Grid
               container
               alignContent="space-around"
-              justifyContent="space-around">
+              justifyContent="space-around"
+            >
               <CircularProgress color="primary" />
             </Grid>
           )}
