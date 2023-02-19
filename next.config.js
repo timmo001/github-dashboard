b2d23c68e9bd73077ba8c346a608f490d8f3a628
Aidan Timson <contact@timmo.xyz>
2023-02-19 13:26:48 +0000

    Recreate

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(graphql|gql|txt)$/,
-      exclude: /node_modules/,
-      loader: "raw-loader",
-    });
-    return config;
-  },
-  webpackDevMiddleware: (config) => {
-    return config;
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
